555067a8b4f01cb6d1a68d57230dd7ddd80978c2
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2018-03-22 14:05:58 -0700

    fixed clean data

--- clean_data.py
+++ clean_data.py
@@ -3,11 +3,11 @@ path = 'mljs/jsnice_data/training_processed.txt'
 with open(path) as train_file:
     train = train_file.readlines()
 train_data = []
-max_context = 20
+max_context = 100
 for t in train:
     ts = json.loads(t)
     if len(ts['input']) <= max_context:
         inp = [[el for el in tt if el!= '0PAD'] for tt in ts['input']]
         train_data.append((inp, ts['output']))
-with open('train_data.json', 'w') as outfile:
+with open('train_data_{}.json'.format(max_context), 'w') as outfile:
     json.dump(train_data, outfile)
