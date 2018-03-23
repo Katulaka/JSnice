6eacd942cb47dd190e267edab2d118508b56ccd0
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2018-03-23 14:17:46 -0700

    gen vocab from freq

---
+++ MLPLVocab.py
@@ -0,0 +1,20 @@
+from torchtext import vocab
+
+class MLPLVocab(vocab.Vocab):
+    def __init__(self, counter, max_size=None, min_freq=1, specials=['<pad>'],
+                 vectors=None, unk_init=None, vectors_cache=None):
+      super(MLPLVocab, self).__init__(counter, max_size=max_size, min_freq=min_freq,
+                                        specials=specials)
+
+    def vtoi(self, val):
+        id = self.stoi.get(val)
+        if id:
+            return id
+        return self.stoi.get('<unk>')
+
+    def itov(self, id):
+        try:
+            return self.itos[id]
+        except:
+            e.args += ('number is not defined')
+            raise
