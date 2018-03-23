69a56e78d7ca25b018fd9b38ad7c5e4e8a9670e9
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2018-03-23 08:27:03 -0700

    creates train and test files

--- clean_data.py
+++ clean_data.py
@@ -1,13 +1,23 @@
 import json
-path = 'mljs/jsnice_data/training_processed.txt'
-with open(path) as train_file:
-    train = train_file.readlines()
-train_data = []
+
+f_train = 'mljs/jsnice_data/training_processed.txt'
+f_test =  'mljs/jsnice_data/eval_processed.txt'
+f_in = [f_train, f_test]
+
 max_context = 100
-for t in train:
-    ts = json.loads(t)
-    if len(ts['input']) <= max_context:
-        inp = [[el for el in tt if el!= '0PAD'] for tt in ts['input']]
-        train_data.append((inp, ts['output']))
-with open('train_data_{}.json'.format(max_context), 'w') as outfile:
-    json.dump(train_data, outfile)
+out_train =  'train_data_{}.json'.format(max_context)
+out_test =  'test_data_{}.json'.format(max_context)
+f_out = [out_train, out_test]
+
+for path_r, path_w in zip(f_in, f_out):
+    with open(path_r) as js_file:
+        data = js_file.readlines()
+        clean_data = []
+        for d in data:
+            ds = json.loads(d)
+            if len(ds['input']) <= max_context:
+                inp = [[el for el in dd if el!= '0PAD'] for dd in ds['input']]
+                clean_data.append((inp, ds['output']))
+
+    with open(path_w, 'w') as outfile:
+        json.dump(clean_data, outfile)
