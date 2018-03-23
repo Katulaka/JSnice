582704e44c68f54a30f2eb7065a1adfb09866413
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2018-03-23 11:19:01 -0700

    Fix

--- clean_data.py
+++ clean_data.py
@@ -1,4 +1,8 @@
 import json
+import itertools
+
+def flatten(l):
+    return list(itertools.chain.from_iterable(l))
 
 MAX_CONTEXT = 100
 # for path_r, path_w in zip(f_in, f_out):
@@ -17,24 +21,27 @@ def clean_data(path_r, path_w):
     return clean_data
 
 def gen_vocab(inp, out):
-    import ipdb; ipdb.set_trace()
     inp_vocab = ['<pad>'] + sorted(list(set(flatten(flatten(inp)))))
-    inp_vocab = dict(map(reversed, enumerate(self.inp_vocab)))
+    inp_vocab = dict(map(reversed, enumerate(inp_vocab)))
     inp_vocab.update({'<unk>':-1})
     print('[gen_vocab] End create input vocab')
     out_vocab = sorted(list(set(out)))
-    out_vocab = dict(map(reversed, enumerate(self.out_vocab)))
+    out_vocab = dict(map(reversed, enumerate(out_vocab)))
     out_vocab.update({'<unk>':-1})
     print('[gen_vocab] End create output vocab')
+    vocab = [inp_vocab, out_vocab]
     with open('vocab', 'w') as outfile:
-        json.dump([inp_vocab, out_vocab], outfile)
+        json.dump(vocab, outfile)
+    return vocab
+
 
 def main():
 
     in_train = 'mljs/jsnice_data/training_processed.txt'
     out_train =  'train_data_{}.json'.format(MAX_CONTEXT)
     tdata = clean_data(in_train, out_train)
-    gen_vocab(*zip(*tdata))
+    vocab = gen_vocab(*zip(*tdata))
+    import ipdb; ipdb.set_trace()
 
     in_eval =  'mljs/jsnice_data/eval_processsed.txt'
     out_eval =  'eval_data_{}.json'.format(MAX_CONTEXT)
