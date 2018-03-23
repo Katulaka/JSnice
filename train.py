b4c76f5faf02c793a59c18a6e021f9d4d7753eef
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2018-03-23 08:27:29 -0700

    add partial code for eval

--- train.py
+++ train.py
@@ -15,8 +15,11 @@ from settings import *
 INIT_LR = 0.01
 BATCH_SIZE = 128
 
+TRAIN = True
+
 NUM_EPOCHS = 400
 PRINT_LOSS_EVERY = 2
+PRINT_ACC_EVERY = 2
 LOG_EVERY = 10
 SAVE_EVERY = 5
 
@@ -76,12 +79,37 @@ def train(ds, model, optimizer=None, iters=None, ds_validate=None, do_log=True):
     if epoch % SAVE_EVERY == 0:
       torch.save(model.state_dict(),'model_epoch_%d.model' % epoch)
 
+def eval(ds, model):
+    sampler = torch.utils.data.sampler.SequentialSampler(ds)
+    testloader = torch.utils.data.DataLoader(ds, batch_size=BATCH_SIZE,
+                                                sampler = sampler,
+                                                collate_fn = mlpl_collate)
+    get_step = lambda x,y: x*len(testloader)+y
+    current_time = time.time()
+    accuracy = 0.
+    for i, (seq, seq_len, labels) in enumerate(testloader):
+        seq = Variable(seq)
+        labels = Variable(labels)
+        if settings['cuda']:
+            seq, seq_len, labels = seq.cuda(), seq_len.cuda(), labels.cuda()
+        outputs = model(seq,seq_len)
+        import ipdb; ipdb.set_trace()
+        # running_accuracy +=
+        # if i % PRINT_ACC_EVERY == PRINT_ACC_EVERY-1:
+        #     new_time = time.time()
+        #     print('Average time per mini-batch, %f' % ((new_time-current_time) / PRINT_LOSS_EVERY))
+        #     current_time = new_time
+        #     print('[%d, %5d] accuracy: %.3f' %
+        #         (epoch + 1, i + 1, running_accuracy / PRINT_LOSS_EVERY))
 
 
 def main():
-    fname = 'train_data_100.json'
-    # import ipdb; ipdb.set_trace()
-    ds = MLPLDataset(fname)
+    if TRAIN:
+        fname = 'train_data.json'
+        ds  = MLPLDataset(fname)
+    else:
+        fname = 'test_data.json'
+        ds  = MLPLDataset(fname)
     print ('[MLPLDataset:] finish init')
     model = MLPLEncoder(len(ds.inp_vocab), len(ds.out_vocab), 32)
     print ('[MLPLDataset:] Load model')
