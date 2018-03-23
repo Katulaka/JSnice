06fd56c836f49e81ca06bdc2c77f71b5d5cb4d16
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2018-03-23 10:53:44 -0700

    debug

--- clean_data.py
+++ clean_data.py
@@ -17,6 +17,7 @@ def clean_data(path_r, path_w):
     return clean_data
 
 def gen_vocab(inp, out):
+    import ipdb; ipdb.set_trace()
     inp_vocab = ['<pad>'] + sorted(list(set(flatten(flatten(inp)))))
     inp_vocab = dict(map(reversed, enumerate(self.inp_vocab)))
     inp_vocab.update({'<unk>':-1})
@@ -33,7 +34,7 @@ def main():
     in_train = 'mljs/jsnice_data/training_processed.txt'
     out_train =  'train_data_{}.json'.format(MAX_CONTEXT)
     tdata = clean_data(in_train, out_train)
-    gen_vocab(zip(*tdata))
+    gen_vocab(*zip(*tdata))
 
     in_eval =  'mljs/jsnice_data/eval_processsed.txt'
     out_eval =  'eval_data_{}.json'.format(MAX_CONTEXT)
